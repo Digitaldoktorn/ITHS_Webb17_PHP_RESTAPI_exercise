db0476956cd084352850bf22cd498b36a3243135
Anders Eriksson <anderse@unseen.is>
2018-09-20 20:21:37 +0200

    Punkt 10 - 11

--- movies.php
+++ movies.php
@@ -31,46 +31,70 @@
     // När vi har å,ä,ö i värden, kan vi behöva sätta charset när vi skapar objektet.
     $dbh = new PDO("mysql: host=localhost; dbname=moviedatabase; charset=utf8", "Anders", "abc123");
 
+    // Lägg till
+    if(isset($_POST['movieTitle'])) {
+        $stmt = $dbh->prepare("
+        INSERT INTO movies
+        (movieTitle, Year, category)
+        VALUES
+        (:mTitle, :year, :cat)
+        ");
 
+        $stmt->bindParam(":mTitle", $_POST['movieTitle']);
+        $stmt->bindParam(":year", $_POST['year']);
+        $stmt->bindParam(":cat", $_POST['category']);
 
+        if($stmt->execute()){
+            $result = $dbh->lastInsertId();
 
+        }
+        else {
+            $result = $stmt->errorInfo();
+        }
+        $result = json_encode($result);
+        echo $result;
+    }
 
-    
-
-    if(isset($_GET['id'])) { // listar alla filmer utan parametrar i URL, dvs inget efter index.php (1 i $idString är som TRUE, se även SQL) 
-
+    else {
         // Kolla om det finns ett ID-värde med.
         if(is_numeric($_GET['id'])){
             $idString = "movies.ID = " . $_GET["id"];
         }
+        
+        if (isset($_GET['title'])){
+            // Kolla om det finns bindesstreck i titeln.
+            if (strpos($_GET['title'], "-") !== false) {
+                $_GET['title'] = str_replace("-", " ", $_GET['title']);
+            }
+            // Ska vi ha LIKE här?
+            $idString = "movieTitle LIKE '" . $_GET['title'] . "%'";
+        }   
 
-        else {
-            echo "Error! ID must be a number";
-            exit;
+        // $idString är antingen tom
+        //eller fylld med "ID=" + ett nummer
+        // eller fylld med movieTitle= + en titel
+        $stmt = $dbh->prepare("
+            SELECT *
+            FROM movies
+            WHERE " . $idString .
+            $sort .
+            $limit
+        );
+
+        if($stmt->execute()) {
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        } else {
+            $result = $stmt->errorInfo();
+            var_dump($stmt);
         }
     }
 
-    // $idString är antingen tom
-    //eller fylld med "ID=" + ett nummer
-    $stmt = $dbh->prepare("
-        SELECT *
-        FROM movies
-        WHERE " . $idString .
-        $sort .
-        $limit
-    );
-
-    if($stmt->execute()) {
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    } else {
-        $result = $stmt->errorInfo();
-        var_dump($stmt);
-    }
-
-    // Om queryn inte hittar något, ge felmeddelande
-    if($result == NULL){
-        echo "Error! No value!";
+    // Kolla om det blev något resultat.
+    if(is_null($result)){
+        $result = [
+            "Beklagar, men vi har ingen film som matchar din sökning: " . $_GET['id'] . $_GET['title']
+        ];
     } 
     
     $result = json_encode($result);
